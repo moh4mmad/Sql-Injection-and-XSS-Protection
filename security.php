923fcc1b7e5e284b80e2071f19c7293850f31843
Mohammad Sakib <moh4mmadsakib@gmail.com>
2017-10-29 21:04:39 +0600

    Create security.php

---
+++ security.php
@@ -0,0 +1,133 @@
+<?php
+function cleaninput($input)
+    {
+        $search = array(
+            '@<script[^>]*?>.*?</script>@si', // Strip out javascript
+            '@<[\/\!]*?[^<>]*?>@si', // Strip out HTML tags
+            '@<style[^>]*?>.*?</style>@siU', // Strip style tags properly
+            '@<![\s\S]*?--[ \t\n\r]*>@' // Strip multi-line comments
+        );
+        
+        $output = preg_replace($search, '', $input);
+        return $output;
+    }
+	
+function sanitize($input)
+    {
+        if (is_array($input)) {
+            foreach ($input as $var => $val) {
+                $output[$var] = sanitize($val);
+            }
+        } else {
+            $input  = str_replace('"', "", $input);
+            $input  = str_replace("'", "", $input);
+            $input  = cleaninput($input);
+            $output = htmlentities($input, ENT_QUOTES);
+        }
+        return @$output;
+    }
+
+        $_POST    = sanitize($_POST);
+        $_GET     = sanitize($_GET);
+        $_REQUEST = sanitize($_REQUEST);
+        $_COOKIE  = sanitize($_COOKIE);
+        if (isset($_SESSION)) {
+            $_SESSION = sanitize($_SESSION);
+        }
+    
+    $request_uri  = $_SERVER['REQUEST_URI'];
+    $query_string = $_SERVER['QUERY_STRING'];
+    
+    $patterns = array(
+        "union",
+        "coockie",
+        "concat",
+        "alter",
+        "table",
+        "from",
+        "where",
+        "exec",
+        "shell",
+        "wget",
+        "**/",
+        "/**",
+        "0x3a",
+        "null",
+        "DR/**/OP/",
+        "drop",
+        "/*",
+        "*/",
+        "*",
+        "--",
+        ";",
+        "||",
+        "'",
+        "' #",
+        "or 1=1",
+        "'1'='1",
+        "BUN",
+        "S@BUN",
+        "char",
+        "OR%",
+        "`",
+        "[",
+        "]",
+        "<",
+        ">",
+        "++",
+        "script",
+        "select",
+        "1,1",
+        "substring",
+        "ascii",
+        "sleep(",
+        "&&",
+        "and",
+        "insert",
+        "between",
+        "values",
+        "truncate",
+        "benchmark",
+        "sql",
+        "mysql",
+        "%27",
+        "%22",
+        "(",
+        ")",
+        "<?",
+        "<?php",
+        "?>",
+        "../",
+        "/localhost",
+        "127.0.0.1",
+        "loopback",
+        ":",
+        "%0A",
+        "%0D",
+        "%3C",
+        "%3E",
+        "%00",
+        "%2e%2e",
+        "input_file",
+        "execute",
+        "mosconfig",
+        "environ",
+        "scanner",
+        "path=.",
+        "mod=.",
+        "eval\(",
+        "javascript:",
+        "base64_",
+        "boot.ini",
+        "etc/passwd",
+        "self/environ",
+        "md5",
+        "echo.*kae",
+        "=%27$"
+    );
+    foreach ($patterns as $pattern) {
+        if (strlen($query_string) > 255 OR strpos(strtolower($query_string), strtolower($pattern)) !== false) {
+		include ("page/blocked.php");
+		exit(1);
+		}
+	}
