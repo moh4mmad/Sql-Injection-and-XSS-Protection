b1915b01f82a16a7ee383d2c8caa864aff1f1a3e
Mohammad Sakib <moh4mmadsakib@gmail.com>
2017-10-29 21:41:14 +0600

    Create blocked.php

---
+++ blocked.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="https://cdn0.iconfinder.com/data/icons/security-double-colour-blue-black-vol-3/52/protection__safety__security__shield-512.png">  
+    <title>Protected by S4K1B</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet" type="text/css">
+</head>
+<body>
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  word-wrap: break-word;
+}
+
+body{
+  background-color:#222;
+  margin-top: 5cm;
+  font-family: 'Changa'; 
+}
+
+div{
+  width:100%;  
+  text-align:center; 
+  text-shadow: 5px 5px 0 rgba(0,0,0,0.2);
+}
+
+h1{
+  color: white;
+  font-weight: 400; 
+  font-size: 300%;
+  letter-spacing: 10px;
+  transition:.2s .3s all;
+}
+
+h1:hover{
+cursor: pointer;
+text-shadow: 0 4px 0 rgba(0,255,0,0.6),4px 4px 0 rgba(0,0,255,0.4),-4px -4px 0 rgba(255,0,0,0.2),0 1px 0 rgba(255,255,0,0.8) ,0 1px 0 rgb(255,255,255), 0 -4px 0 rgba(255,0,255,0.6),0 -2px 0 rgba(255,255,255,0.4);
+  }
+
+h3{
+  font-family: 'Roboto', sans-serif;
+  font-weight: 200;
+  color: white;
+  font-family: 'Changa'; 
+  font-size: 150%;
+  letter-spacing: 6px;
+}
+
+a{
+  display: block;
+  width: 150px;
+  height: 50px;
+  color: white;
+  text-decoration: none;
+  margin: 10px auto 0;
+  transition:.2s .3s all;
+  line-height:50px;
+
+}
+
+a:hover{
+  line-height: 50px;
+  color: #222;
+  text-shadow:none;
+  background-color: rgba(255,255,255,0.9);
+  box-shadow:0 4px 0 0 rgba(0,255,0,0.6),4px 4px 0 rgba(0,0,255,0.4),-4px -4px 0 rgba(255,0,0,0.2),0 1px 0 rgba(255,255,0,0.8) ,0 1px 0 rgb(255,255,255), 0 -4px 0 rgba(255,0,255,0.6),0 -2px 0 rgba(255,255,255,0.4);
+		}
+</style>
+<div class="container">
+
+<h1>Protected By S4K1B</h1>
+<h3>Get this code from <a href="https://github.com/moh4mmad/Sql-Injection-and-XSS-Protection">Github</a></h3>
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>  
